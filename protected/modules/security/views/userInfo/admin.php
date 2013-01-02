0d7b0fdcf0ea914d88e66e7fb1592e2f993e225e
Charles Willoughby <CharlesWilloughby@jis.nashville.org>
2013-01-02 10:15:34 -0600

    Manage Users View
    Small change to the admin.php view

--- protected/modules/security/views/userInfo/admin.php
+++ protected/modules/security/views/userInfo/admin.php
@@ -50,14 +50,11 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'lastname',
 		'middlename',
 		'username',
-		'password',
-		/*
 		'email',
 		'phoneext',
 		'departmentid',
 		'hiredate',
 		'active',
-		*/
 		array(
 			'class'=>'CButtonColumn',
 		),
