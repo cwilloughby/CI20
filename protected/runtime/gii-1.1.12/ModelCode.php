c9a1762ca92a9d26888c5072acb2ffb34df221cb
Charles Willoughby <CharlesWilloughby@jis.nashville.org>
2013-02-19 09:54:55 -0600

    Revert "News Groundwork"
    This reverts commit a19501fafec617988f2b3e6c3f34cd053692dd9a.

--- protected/runtime/gii-1.1.12/ModelCode.php
+++ protected/runtime/gii-1.1.12/ModelCode.php
@@ -3,7 +3,7 @@ return array (
   'template' => 'default',
   'connectionId' => 'db',
   'tablePrefix' => 'ci_',
-  'modelPath' => 'application.models',
+  'modelPath' => 'application.modules.hr.models',
   'baseClass' => 'CActiveRecord',
   'buildRelations' => '1',
 );
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,15 +1,7 @@
 <?php
-/* @var $this NewsController */
-/* @var $dataProvider CActiveDataProvider */
+/* @var $this SiteController */
 
-$this->breadcrumbs=array(
-	'News',
-);
-
-$this->menu=array(
-	array('label'=>'Create News', 'url'=>array('create')),
-	array('label'=>'Manage News', 'url'=>array('admin')),
-);
+$this->pageTitle=Yii::app()->name;
 ?>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
@@ -21,8 +13,3 @@ URL address type "ci" and hit "enter".</p>
 <p>Please create an account under the login if you have not already done so.
 You must be a Criminal Court Clerk employee to create an account. 
 Site functions are disabled until you have logged in.</p>
-
-<?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
-)); ?>
