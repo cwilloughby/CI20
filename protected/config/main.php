445949a1d0dd2502c7252b61d578178b28ce5b6b
Charles Willoughby <CharlesWilloughby@jis.nashville.org>
2013-05-28 12:52:41 -0500

    Strobe Media Player Extension
    Added in the strobe media playback extension.

--- protected/config/main.php
+++ protected/config/main.php
@@ -20,6 +20,7 @@ return array(
 		'application.modules.security.components.*',
 		'application.extensions.phpmailer.JPhpMailer',
 		'application.extensions.jqrelcopy.JQRelcopy',
+		'application.extensions.smp.StrobeMediaPlayback',
 		'application.modules.srbac.controllers.SBaseController',
 		'application.modules.srbac.models.Assignments',
 		'application.modules.email.controllers.*',
