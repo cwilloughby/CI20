a96bcd94d5ee544cd51f57ff4221eed63cb83082
Charles Willoughby <CharlesWilloughby@jis.nashville.org>
2013-06-04 15:52:27 -0500

    Evaluations Module Pagination Tweak
    When an answer to an evaluation question is submitted, the page will now
    automatically change to the next question. The main change is in the
    evaluations controller, but the views provide the get variables.

--- protected/modules/evaluations/views/evaluations/view.php
+++ protected/modules/evaluations/views/evaluations/view.php
@@ -15,7 +15,7 @@ $this->menu2=array(
 	array('label'=>'Create Evaluation', 'url'=>array('create'), 'visible' => Yii::app()->user->checkAccess('Supervisor', Yii::app()->user->id)),
 	array('label'=>'List Evaluations', 'url'=>array('index')),
 	array('label'=>'View Evaluation', 'url'=>array('view', 'id'=>$model->evaluationid)),
-	array('label'=>'Fill Out Evaluation', 'url'=>array('edit', 'id'=>$model->evaluationid), 'visible' => Yii::app()->user->checkAccess('Supervisor', Yii::app()->user->id)),
+	array('label'=>'Fill Out Evaluation', 'url'=>array('edit', 'id'=>$model->evaluationid, 'EvaluationAnswers_page'=>1), 'visible' => Yii::app()->user->checkAccess('Supervisor', Yii::app()->user->id)),
 	array('label'=>'Change Employee', 'url'=>array('update', 'id'=>$model->evaluationid), 'visible' => Yii::app()->user->checkAccess('Supervisor', Yii::app()->user->id)),
 );
 ?>
