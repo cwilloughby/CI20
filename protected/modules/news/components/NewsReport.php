ead06483e1bedb784f46aa94c52e560ddea619b3
Charles Willoughby <CharlesWilloughby@jis.nashville.org>
2013-06-11 10:54:02 -0500

    News Module Widget Polish
    Improved the news widget to show multiple news posts with links.

--- protected/modules/news/components/NewsReport.php
+++ protected/modules/news/components/NewsReport.php
@@ -15,18 +15,39 @@ class NewsReport extends CPortlet
 	{
 		// Grab all the tips and conditionals of the selected subject.
 		$news = Yii::app()->db->createCommand()
-			->select('ci_news_type.type, ci_news.news')
+			->select('ci_news.newsid, ci_news.news, ci_news.date')
 			->from('ci_news_type')
 			->leftJoin('ci_news','ci_news.typeid = ci_news_type.typeid')
 			->where('ci_news_type.type = :type', array(':type'=>$this->type))
 			->order('ci_news.date DESC')
-			->limit(1)
+			->limit(5)
 			->queryAll();
 
 		// Put the news into an array.
-		$news = CHtml::listData($news, 'type', 'news');
+		$newsReport = $this->customTruncate(CHtml::listData($news, 'newsid', 'news'), 100);
+		$newsDates = CHtml::listData($news, 'newsid', 'date');
 		
 		// Display the office news.
-		$this->render('news',array('news'=>$news));
+		$this->render('news',array('news'=>$newsReport, 'type'=>$this->type, 'dates'=>$newsDates));
+	}
+	
+	/**
+	 * This function takes a string and a number for the maximum length.
+	 * If the string is greater than the allowed length, the string will be shortend
+	 * to fit the length and "..." characters will be added to the end of the shortend string.
+	 * @param Array $text
+	 * @param Integer $length
+	 * @return Array
+	 */
+	private function customTruncate($text, $length)
+	{
+		foreach($text as $key => $value)
+		{
+			$length = abs((int)$length);
+			if(strlen($text[$key]) > $length) {
+				$text[$key] = preg_replace("/^(.{1,$length})(\s.*|$)/s", '\\1...', $text[$key]);
+			}
+		}
+		return $text;
 	}
 }
--- protected/modules/news/components/views/news.php
+++ protected/modules/news/components/views/news.php
@@ -1,12 +1,17 @@
 <?php
 /* @var $this NewsController */
 /* @var $news Array */
+/* @var $dates Array */
+/* @var $type String */
 ?>
+
+<h3><?php echo $type; ?></h3>
 
 <?php 
 foreach($news as $key => $value)
 {
-	echo "<h3>" . $key . "</h3>";
-	echo "<br/><p>" . $value . "</p>";
+	echo "<hr/>";
+	echo date('m/d/Y \a\t g:i a', strtotime($dates[$key]));
+	echo "<p>" . CHtml::link($value, array('/news/news/view', 'id'=>$key), array('class'=>'news')) . "</p>";
 }
 ?>
