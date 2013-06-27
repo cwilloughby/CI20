4178b1b17c87ef694d8d23756979cfc239e51515
Charles Willoughby <CharlesWilloughby@jis.nashville.org>
2013-06-27 11:01:53 -0500

    Ugly Typo Correction
    Just a small correction to an ugly typo.

--- protected/modules/timelog/components/views/timereport.php
+++ protected/modules/timelog/components/views/timereport.php
@@ -3,7 +3,7 @@
 /* @var $ciLog Array */
 /* @var $computerLog Array */
 
-echo "You last CI2 login was:<br/>" . CHtml::encode(date('m/d/Y \a\t g:i a', strtotime($ciLog[0]['eventdate'])));
+echo "Your last CI2 login was:<br/>" . CHtml::encode(date('m/d/Y \a\t g:i a', strtotime($ciLog[0]['eventdate'])));
 if(!is_null($computerLog))
 {
 	echo "<br/>Your last known computer login was:<br/>" . CHtml::encode(date('m/d/Y \a\t g:i a', strtotime($computerLog[0]['eventtime'] . " " . $computerLog[0]['eventdate'])));
