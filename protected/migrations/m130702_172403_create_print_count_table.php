a1c2fdc7de1e3cf8b35bb5d7e2d45787029cc927
Charles Willoughby <CharlesWilloughby@jis.nashville.org>
2013-07-02 12:30:33 -0500

    Print Count Table Migration
    Created a migration that added the print count table

---
+++ protected/migrations/m130702_172403_create_print_count_table.php
@@ -0,0 +1,39 @@
+<?php
+
+class m130702_172403_create_print_count_table extends CDbMigration
+{
+	public function __construct()
+	{
+		// Turn the checks on foreign key constraints off before a migration.
+		// This prevents incorrect constraint errors when creating or removing the tables.
+		Yii::app()->db->createCommand('SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;')->execute();
+		Yii::app()->db->createCommand('SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;')->execute();
+		Yii::app()->db->createCommand('SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE="TRADITIONAL";')->execute();
+	}
+	
+	public function __destruct()
+	{
+		// Turn the checks on foreign key constraints back on after a migration.
+		Yii::app()->db->createCommand('SET SQL_MODE=@OLD_SQL_MODE;')->execute();
+		Yii::app()->db->createCommand('SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;')->execute();
+		Yii::app()->db->createCommand('SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;')->execute();
+	}
+	
+	public function up()
+	{
+		$this->createTable('ci_print_count', array(
+				'id' => 'INT NOT NULL AUTO_INCREMENT',
+				'date' => 'DATE NOT NULL',
+				'machineid' => 'INT(2) NULL',
+				'total' => 'INT NULL',
+				'PRIMARY KEY (`id`)',
+			),
+			'ENGINE=InnoDB, COLLATE=utf8_general_ci');
+	}
+
+	public function down()
+	{
+		// Drop the table.
+		$this->dropTable('ci_print_count');
+	}
+}
