135321da00de9663fcd661a5894f37aca69790b2
Charles Willoughby <CharlesWilloughby@jis.nashville.org>
2013-07-03 08:50:50 -0500

    Print Count Chart Error Prevention
    Added a small bit of code to prevent errors on the print chart if
    nothing was returned from the database.

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -73,13 +73,17 @@ class SiteController extends Controller
 		}
 	}
 	
+	/**
+	 * This function is used to obtain the counts from the copiers and printers, for use
+	 * in the chart on the homepage.
+	 */
 	public function actionPrints()
 	{
 		// This snippet will be used later on for obtaining the correct dates.
 		/*
 		$date = new DateTime();
 		$date->add(DateInterval::createFromDateString('yesterday'));
-		echo $date->format('F j, Y') . "\n";
+		echo $date->format('Y-m-d') . "\n";
 		*/
 		
 		$yesterday = Yii::app()->db->createCommand()
@@ -94,6 +98,18 @@ class SiteController extends Controller
 			->where('date=:date', array(':date'=>'2013-07-01'))
 			->queryAll();
 		
+		// These if statements will prevent errors if nothing was returned from the database.
+		if(empty($yesterday))
+		{
+			$yesterday[0]['starttotal'] = 0;
+			$yesterday[0]['endtotal'] = 0;
+		}
+		if(empty($dayBefore))
+		{
+			$dayBefore[0]['starttotal'] = 0;
+			$dayBefore[0]['endtotal'] = 0;
+		}
+			
 		echo json_encode(array(
 			0 => ($yesterday[0]['endtotal'] - $yesterday[0]['starttotal']),
 			1 => ($dayBefore[0]['endtotal'] - $dayBefore[0]['starttotal'])));
