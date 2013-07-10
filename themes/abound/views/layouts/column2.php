d6b9f8bcaeb95427dfe1e051ccf6e39f20d99b41
Charles Willoughby <CharlesWilloughby@jis.nashville.org>
2013-07-10 15:48:53 -0500

    Breadcrumb Fix
    Fixed the breadbrumb path.

--- themes/abound/views/layouts/column2.php
+++ themes/abound/views/layouts/column2.php
@@ -41,7 +41,7 @@
     <?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
             'links'=>$this->breadcrumbs,
-			'homeLink'=>CHtml::link('Home', '/site/index'),
+			'homeLink'=>CHtml::link('Home', '/'),
 			'htmlOptions'=>array('class'=>'breadcrumb')
         )); ?><!-- breadcrumbs -->
     <?php endif?>
