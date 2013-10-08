8555420713a61ea6cf290df2add642b7687118c4
Charles Willoughby <CharlesWilloughby@jis.nashville.org>
2013-10-08 12:47:09 -0500

    Favicon Fixed
    Fixed the favicon in the new theme.

--- themes/urban_city/views/layouts/main.php
+++ themes/urban_city/views/layouts/main.php
@@ -1,61 +1,63 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CI2.0</title>
-<meta name="keywords" content="free website templates, urban city, life, blue, CSS, HTML" />
-<meta name="description" content="Urban City is a free website template from templatemo.com" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta charset="utf-8">
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<?php
+	$baseUrl = Yii::app()->theme->baseUrl; 
+	$cs = Yii::app()->getClientScript();
+	Yii::app()->clientScript->registerCoreScript('jquery');
 
-<?php
-$baseUrl = Yii::app()->theme->baseUrl; 
-$cs = Yii::app()->getClientScript();
-Yii::app()->clientScript->registerCoreScript('jquery');
+	$cs->registerCssFile($baseUrl.'/css/spans.css');
+	$cs->registerCssFile($baseUrl.'/css/responsive_widths.css');
+	$cs->registerCssFile($baseUrl.'/css/abound.css');
+	$cs->registerCssFile($baseUrl.'/css/templatemo_style.css');
+	$cs->registerCssFile($baseUrl.'/css/nivo-slider.css', 'screen');
+	$cs->registerCssFile($baseUrl.'/css/ddsmoothmenu.css');
+	$cs->registerCssFile($baseUrl.'/css/bootstrap.min.css');
+	$cs->registerCssFile($baseUrl.'/css/bootstrap-responsive.min.css');
+	?>
+		
+	<link rel="shortcut icon" href="<?php echo $baseUrl;?>/images/favicon.ico">
+		
+	<script language="javascript" type="text/javascript">
+	function clearText(field)
+	{
+		if (field.defaultValue == field.value) field.value = '';
+		else if (field.value == '') field.value = field.defaultValue;
+	}
+	</script>
 
-$cs->registerCssFile($baseUrl.'/css/spans.css');
-$cs->registerCssFile($baseUrl.'/css/responsive_widths.css');
-$cs->registerCssFile($baseUrl.'/css/abound.css');
-$cs->registerCssFile($baseUrl.'/css/templatemo_style.css');
-$cs->registerCssFile($baseUrl.'/css/nivo-slider.css', 'screen');
-$cs->registerCssFile($baseUrl.'/css/ddsmoothmenu.css');
-$cs->registerCssFile($baseUrl.'/css/bootstrap.min.css');
-$cs->registerCssFile($baseUrl.'/css/bootstrap-responsive.min.css');
-?>
+	<?php
+	$cs->registerScriptFile($baseUrl.'/js/bootstrap.min.js');
+	$cs->registerScriptFile($baseUrl.'/js/jquery.nivo.slider.js');
+	$cs->registerScriptFile($baseUrl.'/js/ddsmoothmenu.js');
+	?>
 
-<script language="javascript" type="text/javascript">
-function clearText(field)
-{
-    if (field.defaultValue == field.value) field.value = '';
-    else if (field.value == '') field.value = field.defaultValue;
-}
-</script>
-
-<?php
-$cs->registerScriptFile($baseUrl.'/js/bootstrap.min.js');
-$cs->registerScriptFile($baseUrl.'/js/jquery.nivo.slider.js');
-$cs->registerScriptFile($baseUrl.'/js/ddsmoothmenu.js');
-?>
-
-<script type="text/javascript">
-$(window).load(function() {
-	$('#slider').nivoSlider({
-		effect:'random',
-		slices:15,
-		animSpeed:600,
-		pauseTime:8000,
-		startSlide:0, // Set starting Slide (0 index)
-		directionNav: false, // Next and Prev
-		directionNavHide:false, // Only show on hover
-		controlNav:false, // 1,2,3...
-		controlNavThumbs:false, //Use thumbnails for Control Nav
-		pauseOnHover:true, //Stop animation while hovering
-		manualAdvance:false, //Force manual transitions
-		captionOpacity:0.8, //Universal caption opacity
-		beforeChange: function(){},
-		afterChange: function(){},
-		slideshowEnd: function(){} //Triggers after all slides have been shown
+	<script type="text/javascript">
+	$(window).load(function() {
+		$('#slider').nivoSlider({
+			effect:'random',
+			slices:15,
+			animSpeed:600,
+			pauseTime:8000,
+			startSlide:0, // Set starting Slide (0 index)
+			directionNav: false, // Next and Prev
+			directionNavHide:false, // Only show on hover
+			controlNav:false, // 1,2,3...
+			controlNavThumbs:false, //Use thumbnails for Control Nav
+			pauseOnHover:true, //Stop animation while hovering
+			manualAdvance:false, //Force manual transitions
+			captionOpacity:0.8, //Universal caption opacity
+			beforeChange: function(){},
+			afterChange: function(){},
+			slideshowEnd: function(){} //Triggers after all slides have been shown
+		});
 	});
-});
-</script>
+	</script>
 
 </head>
 
