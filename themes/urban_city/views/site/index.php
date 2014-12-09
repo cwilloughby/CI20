9e522d655915714ba49c420e7e0e74c3c4d4080f
cwilloughby <CharlesWilloughby@jis.nashville.org>
2014-12-09 14:09:42 -0600

    Homepage Layout Changes
    Altered the layout of the homepage slightly to fit in two new types of
    news.

--- themes/urban_city/views/site/index.php
+++ themes/urban_city/views/site/index.php
@@ -1,80 +1,97 @@
 
 <div class="row-fluid" id="templatemo_wrapper">
 	<div class="templatemo_body">
-		<div class="row-fluid" id="templatemo_middle">
+		<div id="templatemo_middle">
 			<?php
 			if(!isset(Yii::app()->user->id))
 			{
 				?>
-				<div class="span3">
-					<?php $this->widget('UserLogin'); ?>
-				</div>
-				<div class="span6">
-					<h1>Welcome to CI2.0</h1>
-					<p>This is an Intranet website and can only be accessed from a computer 
-					inside the Metro/JIS Domain, just open an internet window and in the
-					URL address type "ci2" and hit "enter".</p>
+				<div class="row-fluid">
 
-					<p>Please create an account under the login if you have not already done so.
-					You must be a Criminal Court Clerk employee to create an account. 
-					Site functions are disabled until you have logged in.</p>
+					<div class="span3">
+						<?php $this->widget('UserLogin'); ?>
+					</div>
+					<div class="span6">
+						<h1>Welcome to CI2.0</h1>
+						<p>This is an Intranet website and can only be accessed from a computer 
+						inside the Metro/JIS Domain, just open an internet window and in the
+						URL address type "ci2" and hit "enter".</p>
 
-					<p>We hope you enjoy the new CI site.</p>
-					<br/>
-					<?php
-					//echo "<h4>12 Hour Forecast</h4>";
-					//$this->widget('WeatherReport');
-					?>
+						<p>Please create an account under the login if you have not already done so.
+						You must be a Criminal Court Clerk employee to create an account. 
+						Site functions are disabled until you have logged in.</p>
+
+						<p>We hope you enjoy the new CI site.</p>
+						<br/>
+						<?php
+						//echo "<h4>12 Hour Forecast</h4>";
+						//$this->widget('WeatherReport');
+						?>
+					</div>
 				</div>
 				<?php
 			}
 			else
 			{
 				?>
-				<div class="span3">
-					<?php
-					echo "<h4>News</h4>";
-					$this->widget('NewsReport');
-					echo "<a href='" . $this->createAbsoluteUrl("/news/news/index") . "'> View All Posts</a>" 
-					?>
-				</div>
-			
-				<div class="span6">
-					<h1>Welcome to CI2.0</h1>
-					<p>This is an Intranet website and can only be accessed from a computer 
-					inside the Metro/JIS Domain, just open an internet window and in the
-					URL address type "ci2" and hit "enter".</p>
+				<div class="row-fluid">
+					<div class="span1">
 
-					<p>Please create an account under the login if you have not already done so.
-					You must be a Criminal Court Clerk employee to create an account. 
-					Site functions are disabled until you have logged in.</p>
+					</div>
+					<div class="span8">
+						<h1>Welcome to CI2.0</h1>
+						<p>This is an Intranet website and can only be accessed from a computer 
+						inside the Metro/JIS Domain, just open an internet window and in the
+						URL address type "ci2" and hit "enter".</p>
 
-					<p>We hope you enjoy the new CI site.</p>
-					<br/>
-					<br/>
-					<br/>
-					<br/>
-					<br/>
-					<br/>
-					<br/>
-					<?php
-					/*
-					echo "<h4>12 Hour Forecast</h4>";
-					$this->widget('WeatherReport');
-					*/
-					?>
-				</div>
+						<p>Please create an account under the login if you have not already done so.
+						You must be a Criminal Court Clerk employee to create an account. 
+						Site functions are disabled until you have logged in.</p>
 
-				<div class="span3">
-					<?php 
-					echo "<h4>IT News</h4>";
-					$this->widget('NewsReport', array('type'=>'IT News'));
-					echo "<a href='" . $this->createAbsoluteUrl("/news/news/index") . "'> View All Posts</a>" 
-					?>
+						<p>We hope you enjoy the new CI site.</p>
+					</div>
+					<div class="span3">
+						<?php
+						echo "<h4>12 Hour Forecast</h4>";
+						$this->widget('WeatherReport');
+						?>
+					</div>
 				</div>
-				<?php
+				<div class="row-fluid">
+					<div class="span3">
+						<?php 
+						echo "<h4>Office News</h4>";
+						$this->widget('NewsReport');
+						echo "<a href='" . $this->createAbsoluteUrl("/news/news/index") . "'> View All Posts</a>" 
+						?>
+					</div>
+					<div class="span3">
+						<?php
+						echo "<h4>Court News</h4>";
+						$this->widget('NewsReport', array('type'=>'Court News'));
+						echo "<a href='" . $this->createAbsoluteUrl("/news/news/index") . "'> View All Posts</a>" 
+						?>
+					</div>
+					<div class="span3">
+						<?php 
+						echo "<h4>Finance News</h4>";
+						$this->widget('NewsReport', array('type'=>'Finance News'));
+						echo "<a href='" . $this->createAbsoluteUrl("/news/news/index") . "'> View All Posts</a>" 
+						?>
+					</div>
+					<div class="span3">
+						<?php 
+						echo "<h4>IT News</h4>";
+						$this->widget('NewsReport', array('type'=>'IT News'));
+						echo "<a href='" . $this->createAbsoluteUrl("/news/news/index") . "'> View All Posts</a>" 
+						?>
+					</div>
+				</div>
+			<?php
 			}
 			?>
 		</div>
 	</div>
 </div>
+
+
